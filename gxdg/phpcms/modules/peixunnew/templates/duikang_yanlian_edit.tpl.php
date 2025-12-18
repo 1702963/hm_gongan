d494c972a9de7772b3aba5df07ab50f2164f1c41
zhangqiang <1702963@qq.com>
2025-12-18 11:19:22 +0800

    feat: 新增 duikang_yanlian 相关文件，包括 PHP 和模板文件

---
+++ gxdg/phpcms/modules/peixunnew/templates/duikang_yanlian_edit.tpl.php
@@ -0,0 +1,101 @@
+<?php
+defined('IN_ADMIN') or exit('No permission resources.');
+include $this->admin_tpl('header_new','admin');
+?>
+
+<SCRIPT LANGUAGE="JavaScript">
+<!--
+parent.document.getElementById('display_center_id').style.display='none';
+//-->
+</SCRIPT>
+
+<script type="text/javascript">
+<!--
+var charset = '<?php echo CHARSET;?>';
+var uploadurl = '<?php echo pc_base::load_config('system','upload_url')?>';
+//-->
+</script>
+<script language="javascript" type="text/javascript" src="<?php echo JS_PATH?>content_addtop.js"></script>
+<script language="javascript" type="text/javascript" src="<?php echo JS_PATH?>colorpicker.js"></script>
+<script language="javascript" type="text/javascript" src="<?php echo JS_PATH?>hotkeys.js"></script>
+<script language="javascript" type="text/javascript" src="<?php echo JS_PATH?>swfupload/swf2ckeditor.js"></script>
+<script language="javascript" type="text/javascript" src="<?php echo JS_PATH?>formvalidator.js" charset="UTF-8"></script>
+<script language="javascript" type="text/javascript" src="<?php echo JS_PATH?>formvalidatorregex.js" charset="UTF-8"></script>
+<link href="<?php echo CSS_PATH?>modelPatch.css?ver=<?php echo time() ?>" rel="stylesheet" type="text/css" />
+
+<form action="?m=peixunnew&c=duikang_yanlian&a=editsave" method="POST" name="myform" id="myform">
+<input type="hidden" name="id" value="<?php echo $this->info['id']?>" />
+
+<div class="tableContent">
+
+<div class="tabcon">
+<div class="title" style="width:auto;">编辑对抗演练</div>
+<table cellpadding="0" cellspacing="0" class="baseinfo" align="center">
+  <tr>
+    <td width="100" align="right" class="infotitle"><span style="color:red">*</span>组织单位：</td>
+    <td colspan="5">
+      <select name="info[bmid]" id="bmid" required
+              style="width:300px;height:28px;background:transparent;color:#fff;border:1px solid #ddd;margin-left:5px;">
+        <option value="0">请选择</option>
+        <?php echo $this->select_bumen?>
+      </select>
+    </td>
+  </tr>
+
+  <tr>
+    <td align="right" class="infotitle"><span style="color:red">*</span>演练标题：</td>
+    <td colspan="5">
+      <input type="text" name="info[title]" id="title" value="<?php echo htmlspecialchars($this->info['title'])?>" required
+             style="width:500px;height:20px;background:transparent;color:#fff;border:1px solid #ddd;margin-left:5px;text-indent:1px"
+             placeholder="请输入对抗演练标题"/>
+    </td>
+  </tr>
+
+  <tr>
+    <td align="right" class="infotitle">演练内容：</td>
+    <td colspan="5">
+      <textarea name="info[content]" id="content" rows="5"
+                style="width:600px;background:transparent;color:#fff;border:1px solid #ddd;margin-left:5px;padding:5px"
+                placeholder="请输入对抗演练的具体内容（支持文字、图片、视频描述）"><?php echo htmlspecialchars($this->info['content'])?></textarea>
+    </td>
+  </tr>
+
+  <tr>
+    <td align="right" class="infotitle">演练评价、总结：</td>
+    <td colspan="5">
+      <textarea name="info[pingjia]" id="pingjia" rows="5"
+                style="width:600px;background:transparent;color:#fff;border:1px solid #ddd;margin-left:5px;padding:5px"
+                placeholder="请输入对抗演练的评价和总结"><?php echo htmlspecialchars($this->info['pingjia'])?></textarea>
+    </td>
+  </tr>
+
+  <tr>
+    <td align="right" class="infotitle">演练状态：</td>
+    <td colspan="5">
+      <select name="info[status]" id="status"
+              style="width:200px;height:28px;background:transparent;color:#fff;border:1px solid #ddd;margin-left:5px;">
+        <option value="1" <?php echo $this->info['status'] == 1 ? 'selected' : ''?>>进行中</option>
+        <option value="2" <?php echo $this->info['status'] == 2 ? 'selected' : ''?>>已完成</option>
+      </select>
+    </td>
+  </tr>
+
+  <tr>
+    <td align="right" class="infotitle"></td>
+    <td colspan="5" style="padding-top:20px;padding-left:5px;">
+      <input type="submit" name="dosubmit" id="dosubmit" value="保存" class="dialog" style="margin-right:10px;font-size:14px;" />
+      <input type="button" value="返回" class="dialog" onclick="javascript:history.back();" />
+    </td>
+  </tr>
+</table>
+</div>
+
+<div class="clear"></div>
+<div class="bk15"></div>
+
+</div>
+
+</form>
+
+</body>
+</html>
---
+++ gxdg/phpcms/modules/peixunnew/templates/duikang_yanlian_list.tpl.php
@@ -0,0 +1,137 @@
+<?php
+defined('IN_ADMIN') or exit('No permission resources.');
+include $this->admin_tpl('header_new', 'admin');
+?>
+
+<SCRIPT LANGUAGE="JavaScript">
+<!--
+parent.document.getElementById('display_center_id').style.display='none';
+//-->
+</SCRIPT>
+
+<style type="text/css">
+html{_overflow-y:scroll}
+.explain-col {
+    border: 1px solid #3132a4;
+    zoom: 1;
+    background: #252682;
+    padding: 8px 10px;
+    line-height: 20px;
+    color:#bbd8f1
+}
+.search-box {
+    margin: 10px 0;
+    padding: 10px;
+    background: #1e1f5e;
+    border-radius: 5px;
+}
+.search-box select, .search-box input[type="text"] {
+    background: transparent;
+    color: #fff;
+    border: 1px solid #ddd;
+    padding: 5px 10px;
+    margin-right: 10px;
+}
+.search-box option {
+    background: #1e1f5e;
+    color: #fff;
+}
+</style>
+<link href="<?php echo CSS_PATH?>modelPatch.css?ver=<?php echo time() ?>" rel="stylesheet" type="text/css" />
+
+<div class="tableContent">
+<div class="pad-lr-10">
+
+<div class="explain-col">
+  快速工具:
+<a href="index.php?m=peixunnew&c=duikang_yanlian&a=add"><input type="button" value="发起对抗演练" style="margin-left:10px; width:100px" class="doLock" name="dook"></a>
+</div>
+
+<div class="search-box">
+    <form method="GET" action="index.php">
+        <input type="hidden" name="m" value="peixunnew">
+        <input type="hidden" name="c" value="duikang_yanlian">
+        <input type="hidden" name="a" value="init">
+
+        <label>组织单位：</label>
+        <select name="bmid" style="width:200px;">
+            <option value="0">全部</option>
+            <?php echo $this->select_bumen?>
+        </select>
+
+        <label style="margin-left:15px;">演练状态：</label>
+        <select name="status" style="width:100px;">
+            <option value="-1">全部</option>
+            <option value="1" <?php echo $this->status == 1 ? 'selected' : ''?>>进行中</option>
+            <option value="2" <?php echo $this->status == 2 ? 'selected' : ''?>>已完成</option>
+        </select>
+
+        <label style="margin-left:15px;">关键词：</label>
+        <input type="text" name="keyword" value="<?php echo htmlspecialchars($this->keyword)?>" placeholder="演练标题或评价" style="width:200px;">
+
+        <input type="submit" value="搜索" class="doLock" style="margin-left:10px;">
+        <a href="index.php?m=peixunnew&c=duikang_yanlian&a=init"><input type="button" value="重置" class="doLock"></a>
+    </form>
+</div>
+
+<div class="table-list">
+<form name="myform" id="myform">
+<script type="text/javascript" >
+  $(document).ready(function() {
+    $(".kotable tbody tr:odd").addClass("odd");
+    $(".kotable tbody tr:even").addClass("even");
+    $(".kotable tbody tr").mouseover(function() {
+      $(this).addClass("iover");
+    }).mouseout(function() {
+      $(this).removeClass("iover");
+    });
+  })
+</script>
+
+<table width="100%" cellspacing="4" cellpadding="4" class="kotable">
+    <thead>
+    <tr>
+      <th width='50'>序号</th>
+      <th width="150">组织单位</th>
+      <th width="200">演练标题</th>
+      <th width="120">状态</th>
+      <th width="120">生成时间</th>
+      <th width="150">评价摘要</th>
+      <th width="100">操作</th>
+    </tr>
+    </thead>
+    <tbody>
+  <?php
+
+if(is_array($this->list)){
+    $i=1;
+
+    foreach($this->list as $info){
+    ?>
+    <tr>
+      <td><?php echo $i?></td>
+      <td><?php echo isset($this->bumen_map[$info['bmid']]) ? $this->bumen_map[$info['bmid']] : '-'?></td>
+      <td><?php echo htmlspecialchars($info['title'])?></td>
+      <td><?php echo $info['status'] == 1 ? '<span style="color:#00ff00">进行中</span>' : '<span style="color:#ffff00">已完成</span>'?></td>
+      <td><?php echo $info['inputtime_show']?></td>
+      <td><?php echo mb_substr($info['pingjia'], 0, 50, 'utf-8') . (mb_strlen($info['pingjia'], 'utf-8') > 50 ? '...' : '')?></td>
+      <td>
+        &nbsp;<a href="index.php?m=peixunnew&c=duikang_yanlian&a=edit&id=<?php echo $info['id']?>">编辑</a>
+        &nbsp;<a href="index.php?m=peixunnew&c=duikang_yanlian&a=del&id=<?php echo $info['id']?>" onclick="javascript:var r = confirm('确认删除该记录吗？');if(!r){return false;}">删除</a>
+      </td>
+    </tr>
+   <?php
+    $i++;}
+}
+?>
+</tbody>
+</table>
+
+<div id="pages"><?php echo $this->pages?></div>
+</form>
+</div>
+</div>
+</div>
+
+</body>
+</html>
